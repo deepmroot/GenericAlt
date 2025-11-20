fa7982a70e7be9f22ab3c7c1e66615ad80c69825
matthewdonsemail-lab <matthewdonsemail@gmail.com>
2025-11-20 12:06:54 +0800

    Set new home route and update navigation links

--- App.tsx
+++ App.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import HomePage from './home/page';
+import HomePage from './GenericAlternativesHome/page';
 import InvestPage from './invest/page';
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<InvestPage />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/invest" element={<InvestPage />} />
-        <Route path="/home" element={<HomePage />} />
       </Routes>
     </Router>
   );
